71c6dae62319b02939dc7b95a05f87951636c355
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2019-03-25 11:57:09 +0700

    Finished project page

--- js/ui-ux.js
+++ js/ui-ux.js
@@ -1,22 +1,3 @@
-function xhttp() {
-
-  const xhttp = new XMLHttpRequest();
-
-  xhttp.open("GET", "json/app.json", true);
-
-  xhttp.onload = () => {
-    if (xhttp.readyState === xhttp.DONE) {
-      if (xhttp.status === 200) {
-
-        // DO THE DOM THING HERE
-
-      }
-    }
-  }
-
-  xhttp.send(null);
-}
-
 $('.carousel').carousel({
   ride: true,
   interval: 1500
